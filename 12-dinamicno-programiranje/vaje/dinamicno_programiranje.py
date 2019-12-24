26a3656989d607079c11ef331908be43d03c587f
Izak <izak.jenko@icloud.com>
2019-12-24 14:37:23 +0100

    python razlicica dinamicnega programiranja

--- 12-dinamicno-programiranje/vaje/dinamicno_programiranje.py
+++ 12-dinamicno-programiranje/vaje/dinamicno_programiranje.py
@@ -5,17 +5,16 @@ def max_cheese(cheese):
     height = len(cheese)
     width = len(cheese[0])
     memo = [[0 for _ in range(width)] for _ in range (height)]
-    # print(memo)
+
     for i in range(height - 1, -1, -1):
         for j in range(width - 1, -1, -1):
+
             dol = 0 if i == height - 1 else memo[i + 1][j]
             desno = 0 if j == width - 1 else memo[i][j + 1]
+
             memo[i][j] = cheese[i][j] + max(desno, dol)
+            
     print(memo)
     return memo[0][0]
 
 max_cheese(test_matrix)
-
-# 9 10 8
-# 7  4 5
-# 8  0 1
