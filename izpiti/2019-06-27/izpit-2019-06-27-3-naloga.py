5786125552e85758677ed50961bc1f695d776e21
Izak <izak.jenko@icloud.com>
2020-01-26 09:59:35 +0100

    primerjal z rešitvami

--- izpiti/2019-06-27/izpit-2019-06-27-3-naloga.py
+++ izpiti/2019-06-27/izpit-2019-06-27-3-naloga.py
@@ -51,3 +51,22 @@ vsa_natovarjanja(4)
 
 k = 3
 print(st_natovarjanj(k) == len(vsa_natovarjanja(k)))
+
+# -------------------------------------------------------
+# uradna rešitev
+
+def count_scenarios(containers, capacity):
+    @lru_cache(maxsize=None)
+    def count_scenarios_aux(k, cap):
+        if cap == 0:
+            return 1
+        dim = len(containers)
+        if k >= dim:
+            return 0
+        n = 0
+        weight = containers[k]
+        if weight <= cap:
+            n += count_scenarios_aux(k, (cap - weight))
+        n += count_scenarios_aux(k+1, cap)
+        return n
+    return (count_scenarios_aux(0, capacity))
