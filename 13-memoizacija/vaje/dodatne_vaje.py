b7a1b1e315e755d0736c2b8ea28bfccd539a57bb
Izak <izak.jenko@icloud.com>
2020-01-27 17:24:25 +0100

    resil robotkov pobeg

--- 13-memoizacija/vaje/dodatne_vaje.py
+++ 13-memoizacija/vaje/dodatne_vaje.py
@@ -46,13 +46,44 @@ soba = [[0, 1, 0, 0, 2],
         [0, 0, 0, 2, 2]]
 
 
-def pobeg(soba, pozicija, koraki):
-    x = pozicija[0]
-    y = pozicija[1]
-    width = len(soba[0])
-    height = len(sobe)
+def pobeg(matrika, vr, st, gorivo):
+    vrstice = len(matrika)
+    stolpci = len(matrika[0])
 
-    def  
+    @lru_cache(maxsize=None)
+    def pobeg_aux(pair, gorivo): 
+        vr = pair[0]
+        st = pair[1]
 
+        if matrika[vr][st] == 1 and gorivo >= 0:
+            return True
+        elif gorivo < 0:
+            return False
+        else:
+            def pogoji(pair):
+                vr = pair[0]
+                st = pair[1]
+                if vr < 0 or vr >= vrstice or st < 0 or st >= stolpci:
+                # vse koordinate matrike bodo od tu naprej v pravilnih mejah
+                    return "zid"
+                elif  matrika[vr][st] == 2:
+                    return "zid"
+                elif matrika[vr][st] == 0 or matrika[vr][st] == 1:
+                    return "prosto"
+                else: # ni druge moznosti
+                    return "zid" 
 
-    return pobeg(soba, gor, koraki - 1) or pobeg(soba, dol, koraki - 1) or pobeg(soba, levo, koraki - 1) or pobeg(soba, desno, koraki - 1)
+#                          levo           dol          desno          gor
+            moznosti = [(vr, st - 1), (vr + 1, st), (vr, st + 1), (vr - 1, st)]
+            situacija = list(map(pogoji, moznosti))
+
+            moznosti_pobega = []
+            for i in [0,1,2,3]:
+                if situacija[i] == "prosto":
+                    moznosti_pobega.append(pobeg_aux(moznosti[i], gorivo - 1))
+
+            return (True in moznosti_pobega)
+    
+    return pobeg_aux((vr, st), gorivo)
+
+print(pobeg(soba, 1, 0, 100))
